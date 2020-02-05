987176550f14a3331ad82e7f0ae558b193620424
Khoa Tran <k.tran.0918@outlook.com>
2020-02-05 02:35:44 -0800

    initial

--- Height Test/ViewController.swift
+++ Height Test/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
+    var character: BodyTrackedEntity?
+    let characterAnchor = AnchorEntity()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.debugOptions = [ARView.DebugOptions.showAnchorGeometry, ARView.DebugOptions.showAnchorOrigins, ARView.DebugOptions.showFeaturePoints, ARView.DebugOptions.showStatistics, ARView.DebugOptions.showWorldOrigin]
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        arView.session.delegate = self
+        
+        let configuration = ARBodyTrackingConfiguration()
+        configuration.automaticSkeletonScaleEstimationEnabled = true
+        arView.session.run(configuration)
+        
+        arView.scene.addAnchor(characterAnchor)
+        
+    }
+    
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
+            print(NSDate())
+            print(bodyAnchor.estimatedScaleFactor)
+        }
+}
 }
